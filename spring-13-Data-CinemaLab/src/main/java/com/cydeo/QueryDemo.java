cc5ab3dacba98e88af6ad7866b7fb1865f73c288
DianaSagoyan <dianasagoyan@yahoo.com>
2022-10-29 20:05:00 -0500

    Completed query tasks in cinema repository(have to solve error)

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/QueryDemo.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/QueryDemo.java
@@ -1,24 +1,37 @@
 package com.cydeo;
 
 import com.cydeo.repository.AccountRepository;
+import com.cydeo.repository.CinemaRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.math.BigInteger;
 
 @Component
 public class QueryDemo implements CommandLineRunner {
 
     private final AccountRepository accountRepository;
+    private final CinemaRepository cinemaRepository;
 
-    public QueryDemo(AccountRepository accountRepository) {
+    public QueryDemo(AccountRepository accountRepository, CinemaRepository cinemaRepository) {
         this.accountRepository = accountRepository;
+        this.cinemaRepository = cinemaRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        System.out.println("------------------Account---------------------");
         System.out.println("Account Query: " + accountRepository.retrieveAccounts());
         System.out.println("Find By Age: " + accountRepository.findByAgeBefore(47));
         System.out.println("Find By Country or state: " + accountRepository.findByCountryOrState("United States", "Virginia"));
         System.out.println("Accounts where age is lower than: " + accountRepository.retrieveAccountsWhereAgeLowerThan(40));
         System.out.println("Age higher than: " + accountRepository.retrieveAgeHigherThan(25));
+
+
+        System.out.println("------------------Cinema---------------------");
+        System.out.println("Cinema by country: " + cinemaRepository.findByLocation_Country("United States"));
+//        System.out.println("Cinema name: " + cinemaRepository.retrieveCinemaWithId(BigInteger.valueOf(10))); //?ERROR
+        System.out.println("Cinema by location: " + cinemaRepository.retrieveCinemaByLocationCountry("United States"));
+        System.out.println("Select distinct: " + cinemaRepository.retrieveDistinct());
     }
 }
--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/CinemaRepository.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/repository/CinemaRepository.java
@@ -2,7 +2,11 @@ package com.cydeo.repository;
 
 import com.cydeo.entity.Cinema;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigInteger;
+import java.util.List;
 
 @Repository
 public interface CinemaRepository extends JpaRepository<Cinema, Long> {
@@ -10,34 +14,44 @@ public interface CinemaRepository extends JpaRepository<Cinema, Long> {
     // ------------------- DERIVED QUERIES ------------------- //
 
     //Write a derived query to get cinema with a specific name
+    List<Cinema> findByName(String name);
 
 
     //Write a derived query to read sorted the top 3 cinemas that contains a specific sponsored name
-
+    List<Cinema> findTop3BySponsoredNameContainsOrderBySponsoredName(String name);
 
     //Write a derived query to list all cinemas in a specific country
-
+    List<Cinema> findByLocation_Country(String country);
 
     //Write a derived query to list all cinemas with a specific name or sponsored name
+    List<Cinema> findByNameOrSponsoredName(String name, String sponsoredName);
 
 
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query to read the cinema name with a specific id
+    @Query("select c.name from Cinema c where c.id = ?1") //?    ERROR
+    String retrieveCinemaWithId(BigInteger id);
 
 
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query to read all cinemas by location country
+    @Query(nativeQuery = true, value = "select * from cinema right join location l on l.country = ?1")
+    List<Cinema> retrieveCinemaByLocationCountry(String country);
 
 
     //Write a native query to read all cinemas by name or sponsored name contains a specific pattern
-
+    @Query(nativeQuery = true, value = "select * from cinema where name contains ?1 or where sponsored_name contains ?1")
+    List<Cinema> retrieveCinemaWithPattern(String pattern);
 
     //Write a native query to sort all cinemas by name
+    @Query(nativeQuery = true, value = "select * from cinema order by name")
+    List<Cinema> sortByName();
 
 
     //Write a native query to distinct all cinemas by sponsored name
-
+    @Query(nativeQuery = true, value = "select distinct sponsored_name from cinema ") //  ?
+    List<String> retrieveDistinct();
 
 }
